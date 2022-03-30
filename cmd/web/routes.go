49649b0c14773e7ec3069dd6bf549232c7354d4c
colestevenson <cole@newmoyergeospatial.com>
2022-03-30 11:39:12 -0400

    lectures 42-44

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,9 +3,10 @@ package main
 import (
 	"bookings-udemy/pkg/config"
 	"bookings-udemy/pkg/handlers"
+	"net/http"
+
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
@@ -17,6 +18,9 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
 
 	return mux
 }
